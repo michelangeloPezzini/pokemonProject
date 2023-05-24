340c598a8e0a74fedf7ec053f20a64cbfb24cad7
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2023-05-24 13:42:44 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+const form = document.querySelector('.form');
+const input_search = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+  );
+  if (APIResponse.status === 200) {
+    //extrai os dados json da response
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
+  const data = await fetchPokemon(pokemon);
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src =
+      data['sprites']['versions']['generation-v']['black-white']['animated'][
+        'front_default'
+      ];
+    input_search.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not Found!';
+    pokemonNumber.innerHTML = '';
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input_search.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon = searchPokemon - 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon = searchPokemon + 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
